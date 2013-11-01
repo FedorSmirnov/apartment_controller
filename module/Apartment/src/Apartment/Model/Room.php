39586ac64825f4b6520d080f454293a38057edd7
FedorSmirnov <fedorsmirnov89@gmail.com>
2013-11-01 18:05:19 +0100

    Added the prepareData function, so that there are 0s instead of nulls after the reading of the database

--- module/Apartment/src/Apartment/Model/Room.php
+++ module/Apartment/src/Apartment/Model/Room.php
@@ -11,6 +11,34 @@ class Room {
 	public $temperature;
 	public $temperature_outside;
 	public $light;
+	public function getPowerSum() {
+		$powerSum = $this->power_light + $this->power_device + $this->power_temperature;
+		
+		return $powerSum;
+	}
+	public function prepareData() {
+		if ($this->power_light == null) {
+			$this->power_light = 0;
+		}
+		if ($this->power_temperature == null) {
+			$this->power_temperature = 0;
+		}
+		if ($this->power_device == null) {
+			$this->power_device = 0;
+		}
+		if ($this->temperature == null) {
+			$this->temperature = 0;
+		}
+		if ($this->power_temperature == null) {
+			$this->power_temperature = 0;
+		}
+		if ($this->temperature_outside == null) {
+			$this->temperature_outside = 0;
+		}
+		if ($this->light == null) {
+			$this->light = false;
+		}
+	}
 	public function exchangeArray($data) {
 		if (! empty ( $data ['apartment_id'] )) {
 			$this->apartment_id = $data ['apartment_id'];
